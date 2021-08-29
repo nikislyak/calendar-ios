47944218283d7301a43c14c518162350c6524b64
Nikita Kislyakov <Nikita_Kislyakov1@epam.com>
2021-08-29 21:33:53 +0300

    Initial Commit

---
+++ DesignSystem/DesignSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignSystemApp.swift
+//  DesignSystem
+//
+//  Created by Nikita Kislyakov1 on 29.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
